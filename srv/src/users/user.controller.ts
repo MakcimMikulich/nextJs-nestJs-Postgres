3738b5f1bd605ee8a4dab0af95e0da1ed797d980
Makcik303123 <adoberoflit@gmail.com>
2024-05-02 23:14:10 +0300

    first commit

---
+++ srv/src/users/user.controller.ts
@@ -0,0 +1,18 @@
+import { UserService } from './users.service';
+import { Controller, Get, Logger } from '@nestjs/common';
+import { UsersResponseDto } from './users.response.dto';
+
+@Controller('users')
+export class UserController {
+  private readonly logger = new Logger(UserController.name);
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async getAllUsers() {
+    console.log(this);
+
+    this.logger.log('Get all users');
+    const users = await this.userService.findAll();
+    return users.map((user) => UsersResponseDto.fromUsersEntity(user));
+  }
+}
